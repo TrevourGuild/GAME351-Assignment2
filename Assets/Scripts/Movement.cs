0c1e5d762c99cf135138349ac39dd63f26d67541
Mudkip993 <103229268+Mudkip993@users.noreply.github.com>
2026-06-12 21:28:26 -0700

    Movement Code Edits
    - Created local variables to help with tilting the hovercraft
    - Code that determines our slope in both FB and LR orientations
    - Code to modify hovercraft tilt with if/else statements to correct for direction facing vs direction tilting

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -6,8 +6,10 @@ public class Movement : MonoBehaviour
 {
     public float moveSpeed = 200f;
     public float turnSpeed = 80f;
-    public float hoverHeight = 4f;
-
+    public float hoverHeight = 10f;
+    public float tiltStrengthMultiplier = 5f; // Lower is better for this variable, otherwise we may sample the wrong spot when we go over sharp hills which causes graphical issues
+    public float tiltDistanceCheck = 6f; // Lower is better for this variable, otherwise we may sample the wrong spot when we go over sharp hills which causese graphical issues
+   
     void Start()
     {
     }
@@ -19,21 +21,81 @@ public class Movement : MonoBehaviour
         float move = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
         float turn = Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime;
 
+//if (Physics.Raycast(transform.position, Vector3.down, out RaycastHit hit))
+        {
+         //   Quaternion surfaceRotation = Quaternion.FromToRotation(Vector3.up, hit.normal);
+
+            //transform.rotation = surfaceRotation;
+        }
+
         // W/S movement
         transform.Translate(0, 0, move);
 
         // A/D turning
         transform.Rotate(0, turn, 0);
 
-        // Keep hovercraft above terrain
+        
         if (terrain != null)
         {
-            Vector3 position = transform.position;
-            float groundHeight = terrain.SampleHeight(position);
+            // Keep hovercraft above terrain
+                Vector3 position = transform.position;
+                float groundHeight = terrain.SampleHeight(position);
 
-            position.y = groundHeight + hoverHeight;
+                position.y = groundHeight + hoverHeight;
 
-            transform.position = position;
+                transform.position = position;
+
+            // Sample and set the slope of the hovercraft to the slope of the terrain below the hovercraft
+
+                // Set local variables needed for determining slope
+                Vector3 _forwardNode = new Vector3(0f, 0f, tiltDistanceCheck);
+                Vector3 _rearNode = new Vector3(0f, 0f, -tiltDistanceCheck);
+                Vector3 _leftNode = new Vector3(-tiltDistanceCheck, 0f, 0f);
+                Vector3 _rightNode = new Vector3(tiltDistanceCheck, 0f, 0f);
+
+                // Determine the slope under our hovercraft
+                float _groundHeightFront = terrain.SampleHeight(position + _forwardNode);
+                float _groundHeightBack = terrain.SampleHeight(position + _rearNode);
+                float _groundSlopeFB = _groundHeightBack - _groundHeightFront;
+
+                float _groundHeightLeft = terrain.SampleHeight(position + _leftNode);
+                float _groundHeightRight = terrain.SampleHeight(position + _rightNode);
+                float _groundSlopeLR = _groundHeightLeft - _groundHeightRight;
+                
+                // Get our current hovercraft angle and save it in a local variable
+                Vector3 currentAngles = transform.eulerAngles;
+                
+                // Determine if we are facing world forward or not
+                bool isFacingWorldForward = Vector3.Dot(transform.forward, Vector3.forward) >= 0f;
+
+                // Determine if we are facing world right or not
+                bool isFacingWorldRight = Vector3.Dot(transform.forward, Vector3.forward) >= 0f; // This will be Vector3.right in the near future but requires the same fix needed for the graphical issues with isFacingWorldForward code switching. Leaving it as .forward for the short term reduces the number of graphical issues to 2 (y rot. -90 and +90) instead of 4.
+
+                // Change our currentAngles variable to be the correct slope, accounting for the direction we are facing
+                if (isFacingWorldForward)
+                {
+                    currentAngles.x = _groundSlopeFB * tiltStrengthMultiplier;
+                }
+                else
+                {
+                    currentAngles.x = -_groundSlopeFB * tiltStrengthMultiplier;
+                }
+                if (isFacingWorldRight)
+                {
+                    currentAngles.z = -_groundSlopeLR * tiltStrengthMultiplier; 
+                }
+                else
+                {
+                    currentAngles.z = _groundSlopeLR * tiltStrengthMultiplier;
+                }
+
+                // Set our hovercraft to the modified currentAngles variable
+                transform.eulerAngles = currentAngles;
+
         }
+
+        
+
+        
     }
 }
